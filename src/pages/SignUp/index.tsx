aa3dec94d703f608f20300d955c7bdbb80a08c40
ronald <aandrade.ronald@gmail.com>
2021-06-08 16:09:39 -0300

    Validando-cadastro(yup)

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from  'yup';
 
 import logoimg from '../../assets/logo.svg';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data: string): void {
-        console.log(data);
-    }
+    const handleSubmit = useCallback (async (data: string) => {
+        try{
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatorio'),
+                email: Yup.string().required('E-mail obrigatorio').email('Digite um e-mail valido'),
+                password: Yup.string().min(6, 'Minimo de 6 digitos'),
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
 
     return (
         <Container>
         <Background />
+
         <Content>
             <img src={logoimg} alt="GoBarber" />
             <Form onSubmit={handleSubmit}>
