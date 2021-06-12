bd2fa086c8448beed0b7c2ab4539f3de567defbf
ronald <aandrade.ronald@gmail.com>
2021-06-12 16:21:53 -0300

    Exibindo-erros-no-input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from  'yup';
 
+import getValidationErrors from '../../utils/getValidationErrors'
 import logoimg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -10,21 +12,36 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+  }
+
 const SignUp: React.FC = () => {
-    const handleSubmit = useCallback (async (data: string) => {
+    const formRef = useRef<FormHandles>(null);
+    const handleSubmit = useCallback(async (data: SignUpFormData ) => {
         try{
+            formRef.current?.setErrors({});
+
             const schema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatorio'),
-                email: Yup.string().required('E-mail obrigatorio').email('Digite um e-mail valido'),
-                password: Yup.string().min(6, 'Minimo de 6 digitos'),
+                email: Yup.string()
+                .required('E-mail obrigatorio')
+                .email('Digite um e-mail valido'),
+                password: Yup.string().min(6, 'Minimo de 6 caracteres'),
             });
+
             await schema.validate(data, {
                 abortEarly: false,
-            })
-        } catch (error) {
-            console.log(error)
+            });
+
+        } catch (err) {
+                const errors = getValidationErrors(err);
+                formRef.current?.setErrors(errors);
+
         }
-    }, [])
+    }, []);
 
     return (
         <Container>
@@ -32,14 +49,15 @@ const SignUp: React.FC = () => {
 
         <Content>
             <img src={logoimg} alt="GoBarber" />
-            <Form onSubmit={handleSubmit}>
-                <h1>Face seu cadastro</h1>
 
-                <Input name="email" icon={FiUser} placeholder="Nome" />
+            <Form ref={formRef} onSubmit={handleSubmit}>
+                <h1>Faça seu cadastro</h1>
+
+                <Input name="name" icon={FiUser} placeholder="Nome" />
                 <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input name="password" icon={FiLock} type="password" placeholder="senha" />
+                <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-                <Button type="submit"> Entrar</Button>
+                <Button type="submit">Cadastrar</Button>
 
             </Form>
 
