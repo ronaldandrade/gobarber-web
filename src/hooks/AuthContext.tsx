cfe7950ed31363c6d8de61697b90f5ea31dbdc7f
ronald <aandrade.ronald@gmail.com>
2021-06-16 14:45:35 -0300

    logout

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -1,38 +1,41 @@
-import React, { createContext, useCallback, useState, useContext} from "react";
+import React,
+ { createContext,
+   useCallback,
+   useState,
+   useContext
+ } from 'react';
 import api from '../services/api';
 
 interface IAuthState {
     token: string;
     user: unknown;
 }
-
-interface   ICredentions {
+interface ICredentions {
     email: string;
     password: string;
 }
-
 interface IAuthContext {
     user: unknown;
-    signIn(credentions:ICredentions): Promise<void>;
+    signIn(credentions: ICredentions): Promise<void>;
+    signOut(): void;
 }
-
 const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
- const AuthProvider: React.FC = ({children}) => {
-    const [ data, setData ] = useState<IAuthState>(() => {
+const AuthProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<IAuthState>(() => {
         const token = localStorage.getItem('@GoBarber:token');
         const user = localStorage.getItem('@GoBarber:user');
 
-        if(token && user ) {
+        if (token && user) {
             return { token, user: JSON.parse(user) };
         }
-        return {} as IAuthState
+        return {} as IAuthState;
     });
 
     const signIn = useCallback(async ({ email, password }) => {
         const response = await api.post('sessions', {
             email,
-            password
+            password,
         });
 
         const { token, user } = response.data;
@@ -40,24 +43,29 @@ const AuthContext = createContext<IAuthContext>({} as IAuthContext);
         localStorage.setItem('@GoBarber:token', token);
         localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
-        setData({ token, user })
-    }, [])
+        setData({ token, user });
+    }, []);
+
+    const signOut = useCallback(() => {
+        localStorage.removeItem('@GoBarber:token');
+        localStorage.removeItem('@GoBarber:user');
+
+        setData({} as IAuthState);
+    }, []);
     return (
-        <AuthContext.Provider value = {{user: data.user, signIn }} >
+        <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
-
-    )
-}
+    );
+};
 
 function useAuth(): IAuthContext {
     const context = useContext(AuthContext);
 
-    if(!context) {
-        throw new Error('useAuth must be userd within an AuthProvider')
+    if (!context) {
+        throw new Error('useAuth must be userd within an AuthProvider');
     }
 
-    return context
-
+    return context;
 }
 export { AuthProvider, useAuth };
