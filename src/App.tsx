d9ab1949877a27ef7e8326e8a50d68d5467b45f6
Ronald <aandrade.ronald@gmail.com>
2021-06-17 15:44:13 -0300

    hook-de-toast

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,18 @@ import SignIn from './pages/SignIn'
 import GlobalStyle from './styles/global';
 
 import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 
 const App: React.FC = () => (
     <>
-    <AuthProvider>
+    <AppProvider>
         <SignIn />
-    </AuthProvider>
+    </AppProvider>
+
     <ToastContainer />
-        <GlobalStyle />
+
+    <GlobalStyle />
     </>
 );
 
