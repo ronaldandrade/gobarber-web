1a0fb294c08b1b8423a5913f47892908cef48e18
ronald <aandrade.ronald@gmail.com>
2021-06-21 18:42:03 -0300

    Configurando-rotas

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,72 +2,80 @@ import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-import * as Yup from  'yup';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
-import getValidationErrors from '../../utils/getValidationErrors'
+import getValidationErrors from '../../utils/getValidationErrors';
 import logoimg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 interface SignUpFormData {
     name: string;
     email: string;
     password: string;
-  }
+}
 
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
-    const handleSubmit = useCallback(async (data: SignUpFormData ) => {
-        try{
+    const handleSubmit = useCallback(async (data: SignUpFormData) => {
+        try {
             formRef.current?.setErrors({});
 
             const schema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatorio'),
                 email: Yup.string()
-                .required('E-mail obrigatorio')
-                .email('Digite um e-mail valido'),
+                    .required('E-mail obrigatorio')
+                    .email('Digite um e-mail valido'),
                 password: Yup.string().min(6, 'Minimo de 6 digitos'),
             });
 
             await schema.validate(data, {
                 abortEarly: false,
             });
-
         } catch (err) {
-                const errors = getValidationErrors(err);
-                formRef.current?.setErrors(errors);
-
+            const errors = getValidationErrors(err);
+            formRef.current?.setErrors(errors);
         }
     }, []);
 
     return (
         <Container>
-        <Background />
+            <Background />
 
-        <Content>
-            <img src={logoimg} alt="GoBarber" />
+            <Content>
+                <AnimationContainer>
+                    <img src={logoimg} alt="GoBarber" />
 
-            <Form ref={formRef} onSubmit={handleSubmit}>
-                <h1>Faça seu cadastro</h1>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu cadastro</h1>
 
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                        <Input name="name" icon={FiUser} placeholder="Nome" />
+                        <Input
+                            name="email"
+                            icon={FiMail}
+                            placeholder="E-mail"
+                        />
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            type="password"
+                            placeholder="Senha"
+                        />
 
-                <Button type="submit">Cadastrar</Button>
+                        <Button type="submit">Cadastrar</Button>
+                    </Form>
 
-            </Form>
-
-            <a href="Login">
-                <FiArrowLeft />
-                Voltar para logon</a>
-        </Content>
-
-
-    </Container>
+                    <Link to="/">
+                        <FiArrowLeft />
+                        Voltar para logon
+                    </Link>
+                </AnimationContainer>
+            </Content>
+        </Container>
     );
 };
 
